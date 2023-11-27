893895535a318ad5688f96b28ac5b6172c98cbe1
Br0deNN <theo.broden@gmail.com>
2023-11-27 22:45:44 +0100

    Add project files.

---
+++ Uppgift 5.1 C/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace C_Uppgift_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många meningar vill du skriva in?");
+            string xm = Console.ReadLine();
+            int xa = int.Parse(xm);
+            string[] xme = new string[xa];
+            for (int i = 0; i < xme.Length; i++)
+            {
+                Console.WriteLine("Mata in en mening");
+                xme[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Här är de meningar du skrev");
+            for (int i = 0;i < xme.Length; i++)
+            {
+                Console.WriteLine(xme[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
